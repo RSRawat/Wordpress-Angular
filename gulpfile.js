d5f2a6ee277b0f3982d74b7cf4724bedeeca1efd
unknown <ravinder.singh.rawat2008@gmail.com>
2015-09-14 21:36:35 +0530

    Configured karma with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,8 @@
 var gulp = require('gulp');
+var KarmaServer = require('karma').Server;
 
-gulp.task();
+gulp.task('test', function(done) {
+	new KarmaServer.start({
+		configFile: __dirname + '/karma.conf.js'
+	}, done);
+});
