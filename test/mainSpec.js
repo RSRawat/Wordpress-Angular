0cbb222c6b47c921d656307a8b777cd9ea20898c
unknown <ravinder.singh.rawat2008@gmail.com>
2015-09-14 20:41:05 +0530

    Setup architecture

---
+++ test/mainSpec.js
@@ -0,0 +1,15 @@
+describe('Testing main.js', function() {
+	// necessary
+	it('newStudent should be object', function() {
+		expect(typeof newStudent).toBe('object');
+	});
+
+	it('newStudent name should be RSR', function() {
+		expect(newStudent.name).toBe('RSR');
+	});
+
+	it('newStudent name should change', function() {
+		newStudent.name = 'jabong';
+		expect(newStudent.name).toBe('jabong');
+	});
+});
